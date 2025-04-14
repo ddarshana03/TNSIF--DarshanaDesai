38bb4a4b4a04d3a73a412ae5589093a9dfb91e2e
Darshana Desai <155078654+ddarshana03@users.noreply.github.com>
2025-04-14 20:46:51 +0530

    Add files via upload

---
+++ Daytwo/ForLoopExample.java
@@ -0,0 +1,25 @@
+
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=50; i++)
+		{
+			System.out.println("Value of i : " + i);
+			
+		}
+	
+
+	}
+
+}
+
+
+
+
+
+//   for(initialization; condition; increment/decrement)
+//   {
+//	   // code
+//   }
---
+++ Daytwo/WhileDemo.java
@@ -0,0 +1,21 @@
+
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		
+     int i=1;
+     while(i<=50)
+     {
+    	 System.out.println("Value of i = "+i);
+    	// i++;
+     }
+	}
+
+}
+
+
+//while(condition)
+//{
+//	//code
+//}

931f08ee4e42942503b76689de57096c3887b264
Darshana Desai <155078654+ddarshana03@users.noreply.github.com>
2025-04-14 21:07:51 +0530

    Add files via upload

---
+++ Daythirteen/ChildThread.java
@@ -0,0 +1,39 @@
+
+
+public class ChildThread  extends Thread{
+
+	private int n;
+	private String msg;
+	
+	public ChildThread(int n, String msg) {
+	
+		this.n = n;
+		this.msg = msg;
+	}
+	
+	@Override
+	public void run()
+	{
+		for(int i=1; i<=n; i++)
+		{
+		
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+			System.out.println("Message "+ msg +" "+ i);
+			
+			//System.out.println(msg + " "+ i + " "+ Thread.currentThread().getName());
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
